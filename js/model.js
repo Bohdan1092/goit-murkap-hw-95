da9503f1738ea0bbfb8577e3d46d69cd084863bf
Valehura Bogdan <101808390+Bohdan1092@users.noreply.github.com>
2022-04-23 19:04:29 +0300

    окно всплывающее

--- js/model.js
+++ js/model.js
@@ -5,7 +5,7 @@ const refs = {
 };
 refs.openModalBtn.addEventListener("click", toggleModal);
 refs.closeModalBtn.addEventListener("click", toggleModal);
-refs.backdrop.addEventListener("click", togBackdropClick);
+refs.backdrop.addEventListener("click", logBackdropClick);
 
 function toggleModal() {
     refs.backdrop.classList.toggle("is-hidden");
